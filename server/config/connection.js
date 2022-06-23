aca405e19ab64d15498d8303b08074d55ba223c4
aldreanelafont <93751029+aldreanelafont@users.noreply.github.com>
2022-06-22 23:30:50 -0700

    db changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mongodb:mongodbpass@cluster0.t2alv.mongodb.net/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
